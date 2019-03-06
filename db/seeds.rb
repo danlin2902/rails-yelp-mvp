63a90ae5d8afcdb818abfd5f464ea2c3c4d5691d
Daniel Lindsey <daniel.lindsey321@gmail.com>
2019-03-06 21:04:41 +0100

    not finished.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant = Restaurant.create(
+  [
+    { name: 'Epicure', address: '75008 Paris', phone_number: '74590340458',  category: 'french' },
+    { name: 'Dominos', address: '123 America', phone_number: '74590340434',  category: 'chinese' },
+    { name: 'Durini', address: '321 Magenta', phone_number: '74590343453',   category: 'italian' },
+    { name: 'Temakihno', address: '111 Cadorna', phone_number: '7459034654', category: 'japanese' },
+    { name: 'McDonalds', address: '111 Milan', phone_number: '7459034087', category: 'italian' }
+
+  ]
+)
